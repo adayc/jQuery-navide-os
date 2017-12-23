120ee6a9a9eec6981a443b5470c7a611374d9be7
romina2911 <romina.hp@gmail.com>
2017-12-23 11:25:36 -0500

    actualizando

--- js/signup.js
+++ js/signup.js
@@ -1,4 +1,17 @@
 $(document).ready(function() {
+    var $email = $('#inputEmail');
+    var $password = $('#inputPassword');
+    
+// asociando eventos a los input
+$email.on('input', function(event) {
+    console.log(event.target.value);
+    console.log($(this).val());
+    var PATERNEMAIL = /^[a-zA-Z0-9\._-]+@[a-zA-Z0-9-]{2,}[.][a-zA-Z]{2,3}$/;
+    console.log(PATERNEMAIL.test($(this).val()));
+
+});
+
+    //console.log($email.val());
 
 
 });
